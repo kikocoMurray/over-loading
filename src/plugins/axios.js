dd8b806e402b4136db7bdf907f0db19e84268048
Murray Chen <murrayc@mega7.fun>
2023-05-05 20:03:16 +0800

    fix: 調整 url 取自env
    (cherry picked from commit f0d0fdfb7a0440ef3b614a7b712779baf38b3e09)

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios"
-import {owner,token} from '../../token.json'
 
 const service = axios.create({
-  baseURL: `https://api.github.com/users/${owner}`,
+  baseURL: `https://api.github.com/users/${import.meta.env.VITE_OWNER}`,
   timeout: 3000,
 })
 
@@ -10,7 +9,7 @@ service.interceptors.request.use(
   (config) => {
     if(config.needToken === true) {
       config.headers = {
-        Authorization: `token ${token}`
+        Authorization: `token ${import.meta.env.VITE_TOKEN}`
       }
     }
     return config
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import Token from './token.json'
+import dotenv from 'dotenv'
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -15,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/github': {
-        target: `https://api.github.com/users/${Token.owner}`, // api 位置
+        target: `https://api.github.com/users/${import.meta.env.VITE_OWNER}`, // api 位置
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
