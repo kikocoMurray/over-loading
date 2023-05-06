60afc3bf852c04d567035f56b168d2aba2183a2c
Murray Chen <murrayc@mega7.fun>
2023-05-06 11:29:08 +0800

    chore: CI

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/over-loading/' : '/',
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/over-loading/' : '/',
   plugins: [vue()],
   server: {
     proxy: {
